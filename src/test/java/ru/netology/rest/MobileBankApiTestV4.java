2d3f520572518a4495e295ac280c7c694b325665
vozle <vozle2@gmail.com>
2022-05-02 23:31:52 +0300

    Update schema test

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -17,7 +17,7 @@ class MobileBankApiTestV4 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(500)
+          .statusCode(200)
           .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
       ;
     }
